0542523873416d407161df45658b8cb47955c46a
github username <github email address>
2019-10-16 13:13:15 -0400

    done

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
   //add static methods here
+
+
+  static capitalize(str){
+    let splitStr = str.split('');
+
+    splitStr[0] = splitStr[0].toUpperCase();
+
+    return splitStr.join('')
+  }
+
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(str){
+    let blacklist = 'the a an but of and for at by from'.split(' ');
+
+    let splitStr = str.split(' ');
+
+    splitStr[0] = this.capitalize(splitStr[0]);
+
+    let newStr;
+
+    newStr = splitStr.map((word) => {
+      if (blacklist.includes(word)){
+        return word;
+      } else {
+        return this.capitalize(word)
+      }
+    })
+    
+    return newStr.join(' ')
+  }
 }
