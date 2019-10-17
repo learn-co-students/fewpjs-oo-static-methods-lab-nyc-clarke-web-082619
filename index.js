6b45183a25d97924c62c95ee8d27ef11e3c66481
castagnaTM <castagna.tm@gmail.com>
2019-10-17 17:11:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(string[0]).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-" "']+/g, '');
+  }
+  static titleize(string){
+    let newString = ''
+      for(const word of string.split(' ')){
+        if (!['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'].includes(word)){
+          newString += ' ' + Formatter.capitalize(word);
+        }
+        else{
+          newString += ' ' + word;
+        }
+      }
+      return Formatter.capitalize(newString.slice(1));
+    }
 }
