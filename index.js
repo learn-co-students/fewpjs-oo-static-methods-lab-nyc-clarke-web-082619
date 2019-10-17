31fa30c541fe7415f4d8543ff43eabcadb6a19d7
github username <github email address>
2019-10-17 11:47:54 -0400

    Done.

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ class Formatter {
  
   static sanitize(string) {
     // removes non-alphanumeric characters except for dash, single quote and space:
-    return string.replace(/[^A-Za-z0-9-'\s]/g, '');
+    return string.replace(/[^A-Za-z0-9-'\s]/g, '');   //       \s == / /
   }
 
-  static titleize(){
-
+  static titleize(string) {
+    let wordArr = string.split(" ");
+    let answer = [];
+    let dontCapitalize = new Set();
+    ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'and', 'from'].forEach(word => dontCapitalize.add(word));
+    for(let i = 0; i < wordArr.length; ++i) {
+      answer.push(dontCapitalize.has(wordArr[i])? wordArr[i] : this.capitalize(wordArr[i])); 
+    }
+    return this.capitalize(answer.join(" "));
   }
 }
